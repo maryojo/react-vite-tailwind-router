a92ac725773297a6ce91d6aa59cbb66171824b17
Mary Ojo <38431519+maryojo@users.noreply.github.com>
2024-09-24 16:08:38 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
